44e7348a97540c5e0e059e8b8d7e672218b3c5d8
Dika Mai Putra <dikamp445@gmail.com>
2022-07-22 07:28:30 +0700

    Routing

--- app/core/App.php
+++ app/core/App.php
@@ -2,12 +2,26 @@
 
 class App{
 
-	protected $controller = 'Home';
-	protected $method = 'index';
-	protected $params = [];
+	// protected $controller = 'Home';
+	// protected $method = 'index';
+	// protected $params = [];
 
 	public function __construct(){
-		echo "Ok!";
+		$url = $this->parseURL();
+		var_dump($url);
+	}
+
+	public function parseURL(){
+		if (isset($_GET['url'])) {
+			//rtrim(str) untuk mengahus string, [nilai yang mau dihapus]
+			$url = rtrim($_GET['url'], '/');
+			//membersihkan url dari carakter yg tidak diperlukan
+			$url = filter_var($url, FILTER_SANITIZE_URL);
+			//pecah url berdasarkan tanda / dengan fungsi explod
+			$url = explode('/', $url);
+			return $url;
+			// code...
+		}
 	}
 
  }
