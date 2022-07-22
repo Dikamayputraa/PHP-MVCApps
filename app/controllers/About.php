a64cef1a6de6eeda03e3bdd79bd7539aaae90fca
Dika Mai Putra <dikamp445@gmail.com>
2022-07-22 07:36:39 +0700

    Controller

---
+++ app/controllers/About.php
@@ -0,0 +1,12 @@
+<?php 
+
+class About{
+	public function index($nama = "Dika Mai Putra", $pekerjaan = "Software Enginer"){
+		echo "Hallo, Nama saya $nama, saya adalah seorang $pekerjaan";
+	}
+	public function page(){
+		echo "About/page";
+	}
+}
+
+ ?>
